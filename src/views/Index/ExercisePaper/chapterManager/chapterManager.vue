f53504d458b170f27e7a66bfc49cba23335a1656
treeFuck <1552660178@qq.com>
2020-09-13 08:24:23 +0800

    9.13 准备拉

---
+++ src/views/Index/ExercisePaper/chapterManager/chapterManager.vue
@@ -0,0 +1,43 @@
+<style scoped lang="scss">
+  .chapterManager {
+    .dataCon {
+      .list {
+
+      }
+    }
+
+
+  }
+</style>
+
+<template>
+  <div class="chapterManager">
+    <div class="dataCon">
+      <div class="list" v-for="(data, index) in chapterArr" :key="index">
+        {{data.chapterName}}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "chapterManager",
+    data() {
+      return {
+        chapterArr: null
+      }
+    },
+    methods: {
+      getChapter() {
+        this.chapterArr = [
+          {chapterId: 106, organizationId: 28, chapterName: "指针", userId: 2},
+          {chapterId: 108, organizationId: 28, chapterName: "初始C语言", userId: 2},
+        ]
+      },
+    },
+    mounted() {
+      this.getChapter();
+    }
+  }
+</script>
